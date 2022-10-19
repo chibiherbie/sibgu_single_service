6c839a5d8b47958fd5c4d3ab948be037915937f7
Roman <bekkerrdm@gmail.com>
2022-10-19 14:51:44 +0700

    collect data from vk
    -collecting data from vk
    -sending a message back from user

--- messengers/vk/vk.py
+++ messengers/vk/vk.py
@@ -1,33 +1,32 @@
 import vk_api
 import requests
 import configparser
+import random
+
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 
 def auth_handler():
-    """ При двухфакторной аутентификации вызывается эта функция"""
+    """При двухфакторной аутентификации вызывается эта функция"""
 
     # Код двухфакторной аутентификации
     key = input("Enter authentication code: ")
-    # Если: True - сохранить, False - не сохранять.
     remember_device = True
 
     return key, remember_device
 
 
-def main():
-    """"""
+def auth():
     # Считываем учетные данные
     config = configparser.ConfigParser()
     config.read("../../config.ini")
 
-    # Присваиваем значения внутренним переменным
     login = config['Vk']['login']
     password = config['Vk']['password']
 
     vk_session = vk_api.VkApi(
-        login, password,
-        # функция для обработки двухфакторной аутентификации
-        auth_handler=auth_handler
+        login, password, app_id=2685278,
+        auth_handler=auth_handler  # функция для обработки двухфакторной аутентификации
     )
 
     try:
@@ -35,6 +34,46 @@ def main():
     except vk_api.AuthError as error_msg:
         print(error_msg)
         return
+
+    return vk_session
+
+
+def send_data(data):
+    print(data)
+
+
+# Отправляем сообщение
+def send_message(vk, user_id, text):
+    vk.messages.send(
+        user_id=user_id,
+        message=text,
+        random_id=random.randint(-100000, +100000),
+        attachment=None,
+        payload=None
+    )
+
+
+def main():
+    vk_session = auth()  # Авторизация
+
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            # Слушаем longpoll, если пришло сообщение то:
+            if event.from_user:  # Если написали в ЛС
+                user = vk.users.get(user_ids=(str(event.user_id)))[0]
+
+                send_data({'id': user['id'],
+                           'username': (user['first_name'], user['last_name']),
+                           'message': event.message,
+                           'date': event.datetime})
+
+                # vk.messages.send(  # Отправляем сообщение
+                #     user_id=event.user_id,
+                #     message='Ваш текст'
+                # )
 
 
 if __name__ == '__main__':
