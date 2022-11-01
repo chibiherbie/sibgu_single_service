f206b2c48fa2647f7b3ec3035c3d5ea9d8d0a348
Roman <bekkerrdm@gmail.com>
2022-11-01 13:11:11 +0700

    user authentication

--- singlechat/user_control/admin.py
+++ singlechat/user_control/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Jwt
 
 
-admin.site.register((CustomUser, ))
+admin.site.register((CustomUser, Jwt))
--- singlechat/user_control/models.py
+++ singlechat/user_control/models.py
@@ -41,3 +41,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.username
+
+
+class Jwt(models.Model):
+    user = models.OneToOneField(
+        CustomUser, related_name="login_user", on_delete=models.CASCADE)
+    access = models.TextField()
+    refresh = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
