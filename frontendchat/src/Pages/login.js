007744e4fa1bf1a7674d92ff0cec52ed017f4541
Roman <bekkerrdm@gmail.com>
2022-12-02 00:31:06 +0700

    create login and register page
    -create folder assets
    
    -create login and register page

--- frontendchat/src/Pages/login.js
+++ frontendchat/src/Pages/login.js
@@ -1,7 +1,38 @@
 import React from "react";
+import eyeopen from "../assets/eyeopen.png"
+import eyeclose from "../assets/eyeclose.png"
+import { Link } from "react-router-dom"
 
 const Login = (props) => {
-    return <div>Login</div>;
+    return (
+        <div className="loginContainer">
+            <div className="inner">
+                <div className="logo">SIBGU</div>
+                <div className="title">Войти</div>
+
+                <AuthForm login/>
+
+                <div className="switchOption">
+                    Нет аккаунта? <Link to="/register">Зарегистрироваться</Link>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export const AuthForm = (props) => {
+    return (
+                        <form>
+                    <input className="input-field" placeholder="Имя пользователя" />
+                    <div className="input-container">
+                        <input className="input-field" placeholder="Пароль" type="password" autoComplete="new-password"/>
+                        <img src={eyeopen}/>
+                        <img src={eyeclose} />
+                    </div>
+                    <button type="submit">Войти</button>
+                </form>
+    );
 };
 
 export default Login;
--- frontendchat/src/Pages/register.js
+++ frontendchat/src/Pages/register.js
@@ -1,7 +1,23 @@
 import React from "react";
+import {AuthForm} from "./login";
+import {Link} from "react-router-dom";
 
 const Register = (props) => {
-    return <div>Register</div>;
+    return (
+        <div className="loginContainer">
+            <div className="inner">
+                <div className="logo">SIBGU</div>
+                <div className="title">Регистрация</div>
+
+                <AuthForm />
+
+                <div className="switchOption">
+                    Уже есть аккаунт? <Link to="/login">Войти</Link>
+                </div>
+
+            </div>
+        </div>
+    );
 };
 
 export default Register;
